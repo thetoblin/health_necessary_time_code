ddf18f9a97eff5c29603d7b7543cbb193381602a
tobka <tobias.lindstrom@protonmail.com>
2022-08-17 16:17:22 +0200

    improve style of minimum-wage barplot (apply whitegrid, change to barplot, fix font sizes, fix image size) and make it ready for public (add subtitle, description and sources)

--- minwage_barplot.py
+++ minwage_barplot.py
@@ -1,6 +1,4 @@
-import matplotlib
-import seaborn
-import pandas
+
 from health_necessary_time_code.minwage_estimate import (
 								 minwage_nectime_weekly_per_capita_df,
 								 minwage_nectime_weekly_perworkingageperson_df,
@@ -9,14 +7,24 @@ from health_necessary_time_code.derived_data import workhours_weekly_avg_df
 from health_necessary_time_code.minwage_settings import year_to_plot, location_name, time_name
 import health_necessary_time_code.auxiliary as aux
 
+# for plotting
+import matplotlib
+import seaborn
+import pandas
+
+# for adding logotype
+from PIL import Image
+
+
 
 def plot(show=True):
 	assert type(show) is bool
 
 	# define legend-texts (visible in plot)
-	minwage_nectime_percapita_legendtext				= 'Time per capita required\nto provide population with\nminimum-wage worth of\ngoods and services'
-	avg_workhours_legendtext							= 'Average working hours'
-	minwage_nectime_perworkingageperson_legendtext		= 'Time per working-age person\nrequired to provide population\nwith minimum-wage worth of\ngoods and services'
+	minwage_nectime_percapita_legendtext				= 'Time required per\ncapita' 				#'Time per capita required\nto provide population with\nminimum-wage worth of\ngoods and services'
+	minwage_nectime_perworkingageperson_legendtext		= 'Time required per\nworking-age person' 	#'Time per working-age person\nrequired to provide population\nwith minimum-wage worth of\ngoods and services'
+	avg_workhours_legendtext							= 'Average working\nhours'
+
 
 
 	# create new dataframes (so to not mess with the originals)
@@ -57,16 +65,78 @@ def plot(show=True):
 	#plot_df = aux.dataframe_remove_rows_with_NaN_values( plot_df )
 
 
-	# plot
-	seaborn.set_theme(style="whitegrid")
-	ax = seaborn.catplot( y='value', x=location_name, hue=time_name, data=plot_df,  ci="sd", palette="dark", alpha=.8, kind="bar", height=8 )
-	#matplotlib.pyplot.subplots_adjust( hspace=1.8 )
-	matplotlib.pyplot.xticks(rotation = 90)
-	matplotlib.pyplot.ylabel("Hours per week")
-	matplotlib.pyplot.savefig('minwage_estimate_plot.png', bbox_inches='tight')
+	###   plot   ###
+	# set figure size
+	matplotlib.pyplot.figure(figsize=(18,9))
 
-	# remove the legend title
-	ax.legend.set_title('')
+	seaborn.set_theme(style="whitegrid")
+	#seaborn.set_theme(style="darkgrid")
+
+	#ax = seaborn.catplot(y='value', x=location_name, hue=time_name, data=plot_df,  ci="sd", palette="deep", alpha=1.0, kind="bar", height=plot_height, aspect=plot_width/plot_height )
+	ax = seaborn.barplot(y='value', x=location_name, hue=time_name, data=plot_df, ci="sd", palette="deep" )
+
+	# fix legend
+	matplotlib.pyplot.legend(frameon=False)
+	seaborn.move_legend(ax, "center right", bbox_to_anchor=(1.175, 0.5))
+
+	# fix ticks and lables
+	matplotlib.pyplot.xlabel(location_name, fontsize=14)
+	matplotlib.pyplot.xticks(rotation = 90)
+	matplotlib.pyplot.ylabel("Hours per week", fontsize=14)
+
+	###   set title, subtitle, and description   ###
+	title_ypos 		= 1.14
+	subtitle_ypos 	= 1.04
+	descr_ypos		= -0.28
+	githublink_ypos	= -0.34
+
+	title_text 		= "Time required to provide population with minimum-wage worth of goods and services (" + str(year_to_plot) + ")"
+	subtitle_text	= "Comparing 1) the number of hours required (per capita and per person in the working-age population (ages 15-64)) to provide everyone in the population\nof a given country with an annual minimum-wage worth of goods and services with the country's average productivity (GDP per hour), with 2) the country's\naverage working hours. For the year " + str(year_to_plot) + "."
+	descr_text		= "Sources:\n     - Robert C. Feenstra, Robert Inklaar. “Penn World Table 10.0.” Groningen Growth and Development Centre, 2021. https://doi.org/10.15141/S5Q94M\n     - Feenstra, Robert C., Robert Inklaar, and Timmer Marcel P. “The Next Generation of the Penn World Table.” American Economic Review 105, no. 10 (October 1, 2015): 3150–82.\n       https://doi.org/10.1257/aer.20130954\n     - OECD. “Minimum Wages at Current Prices in NCU (Database),” December 15, 2021. https://stats.oecd.org/Index.aspx?DataSetCode=MW_CURP (accessed 18 April, 2022)\n     - OECD. “Working Age Population.” Accessed June 29, 2022. https://doi.org/10.1787/d339918b-en"
+	githublink_text	= "MIT License. Source code available at https://github.com/thetoblin/health_necessary_time"
+
+	# title
+	matplotlib.pyplot.title(
+							title_text,
+							y			= title_ypos,
+							fontsize	= 19,
+							loc			= 'left',
+							fontweight	= 'bold',
+							#style		= 'italic',
+							#family		= 'serif'
+							)
+	# subtitle
+	ax.text(x=0.0, y=subtitle_ypos, s=subtitle_text, fontsize=13, ha='left', va='bottom', transform=ax.transAxes) #, family='serif' )#, weight='bold')
+
+	# description
+	ax.text(x=0.0, y=descr_ypos, s=descr_text, fontsize=11, ha='left', va='bottom', transform=ax.transAxes) #, family='serif' )#, weight='bold')
+
+	# githublink and license
+	ax.text(x=0.0, y=githublink_ypos, s=githublink_text, fontsize=11, ha='left', va='bottom', transform=ax.transAxes) #, family='serif' )#, weight='bold')
+
+	# remove top and right border of plot
+	seaborn.despine(ax=ax)
+
+	# add logotype
+	# fig 		= matplotlib.pyplot.gcf()					# get current figure
+	# #a 			= fig.add_subplot(111, frameon=False)
+	# logo 		= matplotlib.pyplot.imread( 'logo.png' )	# read image
+	# logoaxes	= fig.add_axes(								# add new axes (that will be used to scale and position the image)
+	# 							ax.get_position(),
+	# 							label='image',
+	# 							xticks=[], yticks=[],		#remove ticks
+	# 							)
+	# logoaxes.set_xlim()
+	fig 		= matplotlib.pyplot.gcf()					# get current figure
+	logo 		= Image.open('logo.png')					# open logo image file
+	figsize 	= fig.get_size_inches()*fig.dpi				# get the size of the pyplot figure in pixels
+
+	logo_xpos	= figsize[0]-logo.size[0] - 120				# set the logos xposition in pixels
+	logo_ypos	= figsize[1]-logo.size[1] + 200				# set the logos xposition in pixels
+	fig.figimage( logo, xo=logo_xpos, yo=logo_ypos )		# place image in figure
+
+	#save the plot to file
+	matplotlib.pyplot.savefig('minwage_barplot.png', bbox_inches='tight')
 
 	if show:
 		matplotlib.pyplot.show()
